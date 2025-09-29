8023bed8158ba0d019e5da967cab8ee78fbb4aee
Alex Med <lexmedteh@gmail.com>
2025-09-29 17:30:10 +0300

    design fix

--- src/Pages/MoviePage/MoviePage.tsx
+++ src/Pages/MoviePage/MoviePage.tsx
@@ -9,9 +9,9 @@ export const MoviePage = () => {
 
 
   const { id } = useParams()
-  
-  
-  const { data, isLoading } = useGetMovieOrTvDetailQuery({ id:Number(id), tvOrMovie: 'movie' })
+
+
+  const { data, isLoading } = useGetMovieOrTvDetailQuery({ id: Number(id), tvOrMovie: 'movie' })
   const { budget, genres, title, original_language, status, tagline, original_title,
     vote_average, vote_count, runtime, overview, popularity,
     production_countries, production_companies, release_date, backdrop_path, poster_path
@@ -26,88 +26,87 @@ export const MoviePage = () => {
         <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={original_title} />
         <h3>{title}/{original_title}</h3>
       </div>
-      <div className='container-movie-page-info'>
-        <div>
-          <span><b>Жанр: </b></span>
-          {genres?.map((category, idx) => (
-            <span key={idx}>{category.name}</span>
-          ))}
+      
+      <div className='container-movie-page-detail'>
+        <div className='container-movie-page-detail-info'>
+          <div>
+            <span><b>Жанр: </b></span>
+            {genres?.map((category, idx) => (
+              <span key={idx}>{category.name}</span>
+            ))}
+          </div>
+          <div>
+            <span><b>Країна/ни виробник: </b></span>
+            {production_countries?.map((country, idx) => (
+              <span key={idx}>{country.name}</span>
+            ))}
+          </div>
+
+          <div>
+            <span><b>Дата релізу: </b></span>
+            <span>{release_date}</span>
+          </div>
+
+          <div>
+            <span><b>Бюджет: </b></span>
+            <span>{budget}</span>
+          </div>
+
+          <div>
+            <span><b>Мова оригіналу: </b></span>
+            <span>{original_language}</span>
+          </div>
+
+          <div>
+            <span><b>Cтатус: </b></span>
+            <span>{status}</span>
+          </div>
+
+          <div>
+            <span><b>Гасло: </b></span>
+            <span>{tagline}</span>
+          </div>
+
+          <div>
+            <span><b>Тривалість фільму: </b></span>
+            <span>{runtime} хв.</span>
+          </div>
+
+          <div>
+            <span><b>Рейтинг TMDB:</b></span>
+            <span><img src={star} alt="star" /> {popularity}</span>
+
+          </div>
+
+          <div>
+            <span><b>Оцінка глядачів:</b></span>
+            <span>{vote_average}</span>
+          </div>
+
+          <div>
+            <span><b>Кількість голосів:</b></span>
+            <span>{vote_count}</span>
+          </div>
+
+          <div>
+            <span><b>Студія/ії виробники: </b></span>
+
+            {production_companies?.map((studio, idx) => (
+              <div key={idx}>
+                <img src={`https://image.tmdb.org/t/p/w500${studio.logo_path}`} alt="Logo" />
+                <span>{studio.name}</span>
+
+              </div>
+            ))}
+          </div>
+
         </div>
-        <div>
-          <span><b>Країна/ни виробник: </b></span>
-          {production_countries?.map((country,idx) => (
-            <span key={idx}>{country.name}</span>
-          ))}
-        </div>
 
-        <div>
-          <span><b>Дата релізу: </b></span>
-          <span>{release_date}</span>
-        </div>
-
-        <div>
-          <span><b>Бюджет: </b></span>
-          <span>{budget}</span>
-        </div>
-
-        <div>
-          <span><b>Мова оригіналу: </b></span>
-          <span>{original_language}</span>
-        </div>
-
-        <div>
-          <span><b>Cтатус: </b></span>
-          <span>{status}</span>
-        </div>
-
-        <div>
-          <span><b>Гасло: </b></span>
-          <span>{tagline}</span>
-        </div>
-
-        <div>
-          <span><b>Тривалість фільму: </b></span>
-          <span>{runtime} хв.</span>
-        </div>
-
-        <div>
-          <span><b>Рейтинг TMDB:</b></span>
-          <span><img src={star} alt="star" /> {popularity}</span>
-
-        </div>
-
-        <div>
-          <span><b>Оцінка глядачів:</b></span>
-          <span>{vote_average}</span>
-        </div>
-
-        <div>
-          <span><b>Кількість голосів:</b></span>
-          <span>{vote_count}</span>Кількість голосів:28927
-          Студія/ії виробники:
-        </div>
-        
-        <div>
-          <span><b>Студія/ії виробники: </b></span>
-
-          {production_companies?.map((studio, idx) => (
-            <div key={idx}>
-              <img src={`https://image.tmdb.org/t/p/w500${studio.logo_path}`} alt="Logo" />
-              <span>{studio.name}</span>
-
-            </div>
-          ))}
-        </div>
-
-
-
+        <article className='container-movie-page-detail-about'>
+          <p>{overview}</p>
+          <img src={`https://image.tmdb.org/t/p/w500${backdrop_path}`} alt="" />
+        </article>
       </div>
-
-      <article className='container-movie-page-about'>
-        <p>{overview}</p>
-        <img src={`https://image.tmdb.org/t/p/w500${backdrop_path}`} alt="" />
-      </article>
-
     </div>
   )
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -4,41 +4,46 @@ import { PageButton } from './PageButton'
 import './Pagination.scss'
 
 
-export const Pagination = ({ totalPages }:PaginationProps) => {
+export const Pagination = ({ totalPages }: PaginationProps) => {
   const { topMoviesPage, setTopMoviesPage } = useMoviesContext()
 
 
 
   return (
-    <div>
-      <PageButton page={1} current={topMoviesPage} onClick={setTopMoviesPage} />
-      {
-        topMoviesPage != 1 && topMoviesPage != totalPages ? (
-          <>
-            {topMoviesPage - 1 === 1 ? '' : (
-              <>
-                <span>...</span>
-                <PageButton page={topMoviesPage - 1} current={topMoviesPage} onClick={setTopMoviesPage} />
-              </>)}
-            <PageButton page={topMoviesPage} current={topMoviesPage} onClick={setTopMoviesPage} />
-            {topMoviesPage + 1 === totalPages ? '' : (
-              <>
-                <PageButton page={topMoviesPage + 1} current={topMoviesPage} onClick={setTopMoviesPage} />
-                <span>...</span>
-              </>)}
-          </>
-        ) :
-          (<span>...</span>)
-      }
-      <PageButton page={totalPages} current={topMoviesPage} onClick={setTopMoviesPage} />
-      <button
-        disabled={topMoviesPage === 1}
-        onClick={() => setTopMoviesPage(prev => prev - 1)}
-      >Back</button>
-      <button
-        disabled={topMoviesPage === totalPages}
-        onClick={() => setTopMoviesPage(prev => prev + 1)}
-      >Next</button>
+    <div className='pagination'>
+      <div className='pagination-points'>
+        <PageButton page={1} current={topMoviesPage} onClick={setTopMoviesPage} />
+        {
+          topMoviesPage != 1 && topMoviesPage != totalPages ? (
+            <>
+              {topMoviesPage - 1 === 1 ? '' : (
+                <>
+                  <span>...</span>
+                  <PageButton page={topMoviesPage - 1} current={topMoviesPage} onClick={setTopMoviesPage} />
+                </>)}
+              <PageButton page={topMoviesPage} current={topMoviesPage} onClick={setTopMoviesPage} />
+              {topMoviesPage + 1 === totalPages ? '' : (
+                <>
+                  <PageButton page={topMoviesPage + 1} current={topMoviesPage} onClick={setTopMoviesPage} />
+                  <span>...</span>
+                </>)}
+            </>
+          ) :
+            (<span>...</span>)
+        }
+        <PageButton page={totalPages} current={topMoviesPage} onClick={setTopMoviesPage} />
+      </div>
+      
+      <div className='pagination-buttons'>
+        <button
+          disabled={topMoviesPage === 1}
+          onClick={() => setTopMoviesPage(prev => prev - 1)}
+        >Back</button>
+        <button
+          disabled={topMoviesPage === totalPages}
+          onClick={() => setTopMoviesPage(prev => prev + 1)}
+        >Next</button>
+      </div>
     </div>
   )
 }
